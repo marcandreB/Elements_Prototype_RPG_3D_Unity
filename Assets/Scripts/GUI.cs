c1255dba613bbdfcc2e60c38cabf1cd5131ebc9c
Catherine Robitaille <catherine.robitaille.4@ulaval.ca>
2017-11-10 13:37:25 -0500

    Ajout assets et modification GitIgnore

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI : MonoBehaviour
+{
+
+    public Canvas GameCanvas;
+    public Canvas OptionsCanvas; 
+
+    private void Awake()
+    {
+        OptionsCanvas.enabled = false; 
+    }
+
+    public void OptionsOn()
+    {
+        OptionsCanvas.enabled = !(OptionsCanvas.enabled); 
+    }
+
+    public void ReturnOn()
+    {
+        OptionsCanvas.enabled = false;
+    }
+
+    public void quitterJeu()
+    {
+        Application.Quit();
+    }
+
+    public void LoadOn()
+    {
+        Application.LoadLevel(0);
+    }
+}
