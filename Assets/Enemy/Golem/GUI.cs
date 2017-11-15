2221844e926569ca7a8a8005eca4f6baa6515d2c
marcandreB <Marc.andre.brunelle.langevin@hotmail.fr>
2017-11-14 22:33:44 -0500

    Ajout des tours
    Tours.
    Les plateformes pour se rendre un peu plus loin
    
    Bug : Les plateformes bougent sans leur mesh renderer

--- Assets/Enemy/Golem/GUI.cs
+++ Assets/Enemy/Golem/GUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GUI : MonoBehaviour
+public class GUIInterface : MonoBehaviour
 {
 
     public Canvas GameCanvas;
