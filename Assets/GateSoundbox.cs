14a23a58fa7d91552fc940baf48c9a1bd148fdd7
Antoine Beaulieu Lessard <antoine222_@hotmail.com>
2017-11-17 12:32:39 -0500

    Ajouter un son pour la porte

---
+++ Assets/GateSoundbox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GateSoundbox : MonoBehaviour {
+	private AudioSource audioSource;
+
+	public void Start(){
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void BarrierStartMove(){
+		audioSource.Play ();
+	}
+
+	public void BarrierEndMove(){
+		audioSource.Stop ();
+	}
+
+}
