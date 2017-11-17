90573d3733e200f7b1ffd910a0c2882bde9f0087
Antoine Beaulieu Lessard <antoine222_@hotmail.com>
2017-11-17 11:46:23 -0500

    Fait un systeme de gestion des click

---
+++ Assets/Scripts/CubeInteractor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInteractor : MonoBehaviour, IInteractable {
+
+	private Material material;
+
+
+	// Use this for initialization
+	void Start () {
+		material = transform.GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void IInteractable.Interacted (){
+		Destroy (gameObject);
+	}
+
+	void IInteractable.HoverBegin(){
+		material.color = Color.red;
+	}
+
+	void IInteractable.HoverEnd(){
+		material.color = Color.blue;
+	}
+}
