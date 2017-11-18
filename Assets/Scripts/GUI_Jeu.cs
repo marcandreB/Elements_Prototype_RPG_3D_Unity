f43547e8f08970ef5fda96c59b639d7b084cd9f3
Catherine Robitaille <catherine.robitaille.4@ulaval.ca>
2017-11-18 14:56:45 -0500

    Debut Inventaire

--- Assets/Scripts/GUI_Jeu.cs
+++ Assets/Scripts/GUI_Jeu.cs
@@ -1,27 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; 
 
 public class GUI_Jeu : MonoBehaviour
 {
 
     public Canvas GameCanvas;
     public Canvas OptionsCanvas; 
+	public Canvas InventoryCanvas; 
 
     private void Awake()
     {
         OptionsCanvas.enabled = false; 
+		InventoryCanvas.enabled = false; 
     }
+
+	void Update(){
+		if (Input.GetKeyDown ("I"))
+			InventoryOn ();
+	}
 
     public void OptionsOn()
     {
         OptionsCanvas.enabled = !(OptionsCanvas.enabled); 
     }
 
+	public void InventoryOn()
+	{
+		OptionsCanvas.enabled = false; 
+		InventoryCanvas.enabled = !(InventoryCanvas.enabled); 
+	}
+
     public void ReturnOn()
     {
-        OptionsCanvas.enabled = false;
+		OptionsCanvas.enabled = false;
     }
 
     public void quitterJeu()
