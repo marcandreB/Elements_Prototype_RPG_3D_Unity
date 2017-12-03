9b99ebd31e7ce9135769ddd4fb8ee5a935e41e46
marcandreB <Marc.andre.brunelle.langevin@hotmail.fr>
2017-12-02 21:58:53 -0500

    Continuation du niveau de terre + Debut boss
    Ne pas juger, c'est loin d'être terminé
    Ca donne un peu un aperçu, ne vous genez pas pour changer des trucs

---
+++ Assets/Boss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	public GameObject FireBall;
+	public GameObject Player;
+	public GameObject SpawnUp;
+	public GameObject SpawnDown;
+	public float attackDamages;
+	public enum State{Casting, Attacking, Idle, Hidden, BeforeCombat}
+
+	// Use this for initialization
+	void Start () {
+		State = State.BeforeCombat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (State)
+		{
+		case State = State.BeforeCombat:
+			if (Vector3.Distance (playerTransform.position, this.transform.position) < 30)
+				State = State.Idle;
+			break;
+	}
+}
